d263d7a0bedefb9457ccf13a9023bc840b4eaf17
Axel-BeOpenMx <shelonga@live.com>
2021-07-27 20:45:31 -0500

    [FIX] on write: if state changes to declined- create email

--- calendar_decline_activity/models/calendar_attendee.py
+++ calendar_decline_activity/models/calendar_attendee.py
@@ -13,35 +13,38 @@ import time
 class CalendarAttendee(models.Model):
     _inherit = 'calendar.attendee'
 
-    def do_decline(self):
-        res = super(CalendarAttendee, self).do_decline()
-        for attendee in self:
-            if not self.env.company.sudo().calendar_decline_manager_ids:
-                continue
-            manager_partner = self.env.company.sudo().calendar_decline_manager_ids.partner_id.ids
-            body = _("<b>%s: %s</b> has declined the invitation.<br/>date: %s") % (attendee.event_id.name,
-                                                                               attendee.common_name,
-                                                                               fields.Date.context_today(self))
-            self.env['mail.message'].sudo().create({
-                'author_id': self.env.user.partner_id.id,
-                'date': time.strftime('%Y-%m-%d %H:%M:%S'),
-                'message_type': 'comment',
-                'record_name': attendee.event_id.name,
-                'model': 'calendar.event',
-                'res_id': attendee.event_id.id,
-                'partner_ids': [(6, 0, manager_partner)],
-                'notification_ids': [(0, 0, {
-                    'notification_type': 'email',
-                    'res_partner_id': partner,
-                }) for partner in manager_partner],
-                'subject': body,
-                'body': body,
-            })
-            send_mail = self.env['mail.mail'].sudo().create({
-                'subject': _('Calendar event: %s , declined the invitation.') % attendee.event_id.name,
-                'author_id': self.env.user.partner_id.id,
-                'body_html': body,
-                'recipient_ids': [(6, 0, manager_partner)],
-            })
-            send_mail.sudo().send()
+    def write(self, vals):
+        res = super().write(vals)
+        if vals.get('state'):
+            if vals.get('state') == 'declined':
+                for attendee in self:
+                    if not self.env.company.sudo().calendar_decline_manager_ids:
+                        continue
+                    manager_partner = self.env.company.sudo().calendar_decline_manager_ids.partner_id.ids
+                    body = _("<b>%s: %s</b> has declined the invitation.<br/>date: %s") % (attendee.event_id.name,
+                                                                                           attendee.common_name,
+                                                                                           fields.Date.context_today(
+                                                                                               self))
+                    self.env['mail.message'].sudo().create({
+                        'author_id': self.env.user.partner_id.id,
+                        'date': time.strftime('%Y-%m-%d %H:%M:%S'),
+                        'message_type': 'comment',
+                        'record_name': attendee.event_id.name,
+                        'model': 'calendar.event',
+                        'res_id': attendee.event_id.id,
+                        'partner_ids': [(6, 0, manager_partner)],
+                        'notification_ids': [(0, 0, {
+                            'notification_type': 'email',
+                            'res_partner_id': partner,
+                        }) for partner in manager_partner],
+                        'subject': body,
+                        'body': body,
+                    })
+                    send_mail = self.env['mail.mail'].sudo().create({
+                        'subject': _('Calendar event: %s , declined the invitation.') % attendee.event_id.name,
+                        'author_id': self.env.user.partner_id.id,
+                        'body_html': body,
+                        'recipient_ids': [(6, 0, manager_partner)],
+                    })
+                    send_mail.sudo().send()
         return res
